1b6c94d421ea0275d56b83199b31eb45ba56a416
CaioVicenzi <80120724+CaioVicenzi@users.noreply.github.com>
2024-06-10 21:55:32 -0300

    feat:json-util

--- src/main/java/org/library/resource/JsonUtil.java
+++ src/main/java/org/library/resource/JsonUtil.java
@@ -1,4 +1,27 @@
 package org.library.resource;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
 public class JsonUtil {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> T readJsonFile(String filePath, Class<T> clazz) throws IOException {
+        URL resource = JsonUtil.class.getClassLoader().getResource(filePath);
+        if (resource == null) {
+            throw new FileNotFoundException("File not found: " + filePath);
+        }
+        File file = new File(resource.getFile());
+        return objectMapper.readValue(file, clazz);
+    }
+
+    public static <T> void writeJsonFile(String filePath, T object) throws IOException {
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/" + filePath), object);
+    }
 }
